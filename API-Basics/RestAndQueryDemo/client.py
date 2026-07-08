4ccc418d635e97eb05dfb862452c63fb36a3b8b0
Arman Rizvi <210411543+Alpha241026@users.noreply.github.com>
2026-07-08 16:46:43 +0530

    added post method to server and client programs

--- API-Basics/RestAndQueryDemo/client.py
+++ API-Basics/RestAndQueryDemo/client.py
@@ -6,3 +6,27 @@ if r.status_code==200:
     print(r.json())
 else:
     print("Issue retrieving information...")
+
+choice = input("Do you want to enter a new book record ? (y/n)\n")
+
+if choice=="y":
+    title = input("Enter title : ")
+    category = input("Enter category : ")
+    price = input("Enter price : ")
+    stock = input("Enter stock : ")
+    s = r.json()
+    idlist = [book["id"] for book in s]
+
+    new_book = {"id": max(idlist)+1, "title": title, "category": category, "price": int(price), "stock": int(stock)}
+
+    r = requests.post("http://127.0.0.1:5000/books",json=new_book)
+
+    if r.status_code==201:
+        print(r.text)
+    else:
+        print("Error adding new book record !")
+        
+elif choice=="n":
+    pass
+else:
+    print("Invalid input !")
--- API-Basics/RestAndQueryDemo/flask_server.py
+++ API-Basics/RestAndQueryDemo/flask_server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify #pullig in Flask class from flask package...and jsonify helper function
+from flask import Flask, request, jsonify #pullig in Flask class from flask package...and request & jsonify helper functions
 
 app = Flask(__name__) #boilerplate for creating actual app instance
 
@@ -18,6 +18,12 @@ books = [
 @app.route("/books",methods=["GET"]) #decorator to register below function as handler for any request hitting the url path "/books"...also note that this would've run just with the url path as @app.route defaults to handling GET
 def get_books():
     return jsonify(books) #returning dictionary in a json format when get route called from client side
+
+@app.route("/books", methods=["POST"]) #decorator to register below func as handler for any request creating a new book record from the url path
+def post_book():
+    r = request.get_json() #converting incoming json back to Python dictionary
+    books.append(r) #adding new record
+    return jsonify({"message" : "Book added successfully!"}), 201 #returning success message and status code
 
 if __name__ == "__main__": #to make sure file run only when executed directly
     app.run(debug=True) #starts server and auto reloads on updates
